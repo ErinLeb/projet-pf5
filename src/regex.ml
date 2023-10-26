c9776198da87971e9ff41698e1f6c1b924bf8404
Clément <clef-men@orange.fr>
2023-10-26 22:51:26 +0200

    Corrections.

--- src/regex.ml
+++ src/regex.ml
@@ -1,22 +1,31 @@
 open Regex_base
 
-let rec repeat n l = failwith "À compléter"
+let rec repeat n l =
+  failwith "À compléter"
 
-let rec expr_repeat n e = failwith "À compléter"
+let rec expr_repeat n e =
+  failwith "À compléter"
 
-let rec is_empty = failwith "À compléter"
+let rec is_empty e =
+  failwith "À compléter"
 
-let rec null = failwith "À compléter"
+let rec null e =
+  failwith "À compléter"
 
-let rec is_finite e = failwith "À compléter"
+let rec is_finite e =
+  failwith "À compléter"
 
-let product l1 l2 = failwith "À compléter"
+let product l1 l2 =
+  failwith "À compléter"
 
-let enumerate alphabet e = failwith "À compléter"
+let enumerate alphabet e =
+  failwith "À compléter"
 
-let rec alphabet_expr = failwith "À compléter"
+let rec alphabet_expr e =
+  failwith "À compléter"
 
 type answer =
   Infinite | Accept | Reject
 
-let accept_partial e w = failwith "À compléter"
+let accept_partial e w =
+  failwith "À compléter"
