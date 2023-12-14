9fcd491f65cfdb6acbb8f5278d1e27d6af4df62c
LE BOULC'H erin <erin.le-boulch@etu.u-paris.fr>
2023-12-14 23:06:46 +0100

    accept_partial

--- src/regex.ml
+++ src/regex.ml
@@ -1,5 +1,6 @@
 open Regex_base
 
+(* Renvoie l concaténé n fois avec lui même *)
 let repeat n l = 
   if n <= 0 then [] else 
   let rec aux n res = 
@@ -7,7 +8,7 @@ let repeat n l =
     else aux (n-1) l@res 
   in aux n l;;
 
-
+(* Renvoie une expression régulière qui reconnaît les mots formés de la concaténation de n mots reconnus par e *)
 let expr_repeat n e =
   if n<= 0 then Eps else
   if n = 1 then e else
@@ -16,6 +17,7 @@ let expr_repeat n e =
     else aux (n-1) (Concat (e, res))
     in aux n e ;;
 
+(* Renvoie true si le langage reconnu par e ne contient que le mot vide, false sinon *)
 let rec is_empty e = match e with
 | Eps -> true
 | Joker | Base _  -> false
@@ -23,6 +25,7 @@ let rec is_empty e = match e with
 | Concat (a,b) | Alt (a, b) ->
     is_empty a && is_empty b
 
+(* Renvoie true si le mot vide est reconnu par e, false sinon *)
 let rec null e = match e with
 | Eps -> true
 | Joker | Base _  -> false
@@ -30,14 +33,14 @@ let rec null e = match e with
 | Concat (a,b) -> null a && null b
 | Alt (a, b) -> null a || null b
 
+(* Renvoie true si le langage reconnu par e est fini, false sinon *)
 let rec is_finite e =
   match e with 
   | Eps | Joker | Base _ -> true
   | Concat (a,b) | Alt (a,b) -> (is_finite a) && (is_finite b)
   | Star x -> is_empty x
 
-
-
+(* Renvoie l'ensemble des mots formés de la concaténation de l1 et l2 *)
 let product l1 l2 =
   let rec aux res reste = 
     match reste with 
@@ -46,6 +49,7 @@ let product l1 l2 =
   in aux [] l1
   ;;
 
+(* Renvoie none si le langage reconnu par e sur alphabet est infini, Some l le langage reconnu sinon *)
 let rec enumerate alphabet e =
   if not (is_finite e) then None
   else let rec aux e = match e with
@@ -57,7 +61,7 @@ let rec enumerate alphabet e =
   | Alt (a, b) -> (aux a) @ (aux b)
   in Some (List.sort_uniq compare (aux e))
 
-
+(* Renvoie la liste triée des lettres de e *)
 let alphabet_expr e =
   let rec aux e = match e with
   | Eps | Joker -> []
@@ -69,5 +73,11 @@ let alphabet_expr e =
 type answer =
   Infinite | Accept | Reject
 
+(* Renvoie Infinite si le langage reconnu par e est infini, Accept s'il reconnait w, Reject sinon *)
 let accept_partial e w =
-  failwith "À compléter"
+  let alphabet_e = alphabet_expr e in
+  let alphabet = List.sort_uniq compare (alphabet_e)@(w) in
+  let langage = enumerate alphabet e in 
+  match langage with 
+    |None -> Infinite
+    |Some l -> if (List.mem w l) then Accept else Reject
